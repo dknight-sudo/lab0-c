14e2e03b08b312b69de46ae0b918fb2cbbd5da83
ayy77v <zm7484938@gmail.com>
2021-06-10 20:05:27 +0800

    Implement q_insert_tail()

--- queue.c
+++ queue.c
@@ -74,10 +74,25 @@ bool q_insert_head(queue_t *q, char *s)
  */
 bool q_insert_tail(queue_t *q, char *s)
 {
-    /* TODO: You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    if (!q)
+        return false;
+    list_ele_t *newt = malloc(sizeof(list_ele_t));
+    if (!newt)
+        return false;
+    newt->value = strdup(s);
+    if (!newt->value) {
+        free(newt);
+        return false;
+    }
+    newt->next = NULL;
+    if (q->tail) {
+        q->tail->next = newt;
+        q->tail = newt;
+    } else {
+        q->head = q->tail = newt;
+    }
+    q->size += 1;
+    return true;
 }
 
 /*
